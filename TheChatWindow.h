e1d444b7a240b61587b65dd73404461d72c6fa94
Вадим <ddd.ddd99.99@mail.ru>
2020-05-02 21:06:15 +0300

    Начало обработки 1 кнопки и 2 вкладки

--- TheChatWindow.h
+++ TheChatWindow.h
@@ -34,24 +34,38 @@ namespace ClientWinForms {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::TabControl^ tabControl1;
-	protected:
-	private: System::Windows::Forms::TabPage^ tabPage1;
-	private: System::Windows::Forms::TextBox^ infoTextBox;
-
-	private: System::Windows::Forms::Button^ exit;
-
-	private: System::Windows::Forms::Button^ usersOnline;
-
-	private: System::Windows::Forms::Button^ oldChats;
-
-	private: System::Windows::Forms::Button^ newChat;
-
 	private: System::Windows::Forms::TabPage^ tabPage2;
+	protected:
 	private: System::Windows::Forms::TextBox^ usersInChat;
-	private: System::Windows::Forms::Button^ sendMessage;
 	private: System::Windows::Forms::TextBox^ oldMessages;
 	private: System::Windows::Forms::TextBox^ newMessage;
+	private: System::Windows::Forms::Button^ sendMessage;
+	private: System::Windows::Forms::TabControl^ tabControl1;
+	private: System::Windows::Forms::TabPage^ tabPage1;
+	private: System::Windows::Forms::TextBox^ infoTextBox;
+	private: System::Windows::Forms::Button^ exit;
+	private: System::Windows::Forms::Button^ usersOnline;
+	private: System::Windows::Forms::Button^ oldChats;
+	private: System::Windows::Forms::Button^ newChat;
+	private: System::Windows::Forms::TextBox^ ChatUsers;
+
+	protected:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	private:
 		/// <summary>
@@ -66,22 +80,74 @@ namespace ClientWinForms {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
+			this->usersInChat = (gcnew System::Windows::Forms::TextBox());
+			this->oldMessages = (gcnew System::Windows::Forms::TextBox());
+			this->newMessage = (gcnew System::Windows::Forms::TextBox());
+			this->sendMessage = (gcnew System::Windows::Forms::Button());
 			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
 			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
-			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
-			this->newChat = (gcnew System::Windows::Forms::Button());
-			this->oldChats = (gcnew System::Windows::Forms::Button());
-			this->usersOnline = (gcnew System::Windows::Forms::Button());
-			this->exit = (gcnew System::Windows::Forms::Button());
+			this->ChatUsers = (gcnew System::Windows::Forms::TextBox());
 			this->infoTextBox = (gcnew System::Windows::Forms::TextBox());
-			this->newMessage = (gcnew System::Windows::Forms::TextBox());
-			this->oldMessages = (gcnew System::Windows::Forms::TextBox());
-			this->sendMessage = (gcnew System::Windows::Forms::Button());
-			this->usersInChat = (gcnew System::Windows::Forms::TextBox());
+			this->exit = (gcnew System::Windows::Forms::Button());
+			this->usersOnline = (gcnew System::Windows::Forms::Button());
+			this->oldChats = (gcnew System::Windows::Forms::Button());
+			this->newChat = (gcnew System::Windows::Forms::Button());
+			this->tabPage2->SuspendLayout();
 			this->tabControl1->SuspendLayout();
 			this->tabPage1->SuspendLayout();
-			this->tabPage2->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// tabPage2
+			// 
+			this->tabPage2->Controls->Add(this->usersInChat);
+			this->tabPage2->Controls->Add(this->oldMessages);
+			this->tabPage2->Controls->Add(this->newMessage);
+			this->tabPage2->Controls->Add(this->sendMessage);
+			this->tabPage2->Location = System::Drawing::Point(4, 25);
+			this->tabPage2->Name = L"tabPage2";
+			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage2->Size = System::Drawing::Size(712, 403);
+			this->tabPage2->TabIndex = 1;
+			this->tabPage2->Text = L"Chat1";
+			this->tabPage2->UseVisualStyleBackColor = true;
+			// 
+			// usersInChat
+			// 
+			this->usersInChat->Enabled = false;
+			this->usersInChat->Location = System::Drawing::Point(596, 14);
+			this->usersInChat->Multiline = true;
+			this->usersInChat->Name = L"usersInChat";
+			this->usersInChat->ReadOnly = true;
+			this->usersInChat->Size = System::Drawing::Size(105, 300);
+			this->usersInChat->TabIndex = 3;
+			// 
+			// oldMessages
+			// 
+			this->oldMessages->Enabled = false;
+			this->oldMessages->Location = System::Drawing::Point(10, 15);
+			this->oldMessages->Multiline = true;
+			this->oldMessages->Name = L"oldMessages";
+			this->oldMessages->ReadOnly = true;
+			this->oldMessages->Size = System::Drawing::Size(575, 300);
+			this->oldMessages->TabIndex = 1;
+			// 
+			// newMessage
+			// 
+			this->newMessage->Location = System::Drawing::Point(10, 326);
+			this->newMessage->Multiline = true;
+			this->newMessage->Name = L"newMessage";
+			this->newMessage->Size = System::Drawing::Size(575, 67);
+			this->newMessage->TabIndex = 0;
+			// 
+			// sendMessage
+			// 
+			this->sendMessage->Location = System::Drawing::Point(596, 327);
+			this->sendMessage->Name = L"sendMessage";
+			this->sendMessage->Size = System::Drawing::Size(106, 65);
+			this->sendMessage->TabIndex = 2;
+			this->sendMessage->Text = L"Отправить";
+			this->sendMessage->UseVisualStyleBackColor = true;
 			// 
 			// tabControl1
 			// 
@@ -96,6 +162,7 @@ namespace ClientWinForms {
 			// 
 			// tabPage1
 			// 
+			this->tabPage1->Controls->Add(this->ChatUsers);
 			this->tabPage1->Controls->Add(this->infoTextBox);
 			this->tabPage1->Controls->Add(this->exit);
 			this->tabPage1->Controls->Add(this->usersOnline);
@@ -109,55 +176,14 @@ namespace ClientWinForms {
 			this->tabPage1->Text = L"Menu";
 			this->tabPage1->UseVisualStyleBackColor = true;
 			// 
-			// tabPage2
+			// ChatUsers
 			// 
-			this->tabPage2->Controls->Add(this->usersInChat);
-			this->tabPage2->Controls->Add(this->sendMessage);
-			this->tabPage2->Controls->Add(this->oldMessages);
-			this->tabPage2->Controls->Add(this->newMessage);
-			this->tabPage2->Location = System::Drawing::Point(4, 25);
-			this->tabPage2->Name = L"tabPage2";
-			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
-			this->tabPage2->Size = System::Drawing::Size(712, 403);
-			this->tabPage2->TabIndex = 1;
-			this->tabPage2->Text = L"Chat1";
-			this->tabPage2->UseVisualStyleBackColor = true;
-			// 
-			// newChat
-			// 
-			this->newChat->Location = System::Drawing::Point(25, 40);
-			this->newChat->Name = L"newChat";
-			this->newChat->Size = System::Drawing::Size(110, 40);
-			this->newChat->TabIndex = 0;
-			this->newChat->Text = L"Новый чат";
-			this->newChat->UseVisualStyleBackColor = true;
-			// 
-			// oldChats
-			// 
-			this->oldChats->Location = System::Drawing::Point(25, 135);
-			this->oldChats->Name = L"oldChats";
-			this->oldChats->Size = System::Drawing::Size(110, 40);
-			this->oldChats->TabIndex = 1;
-			this->oldChats->Text = L"Список чатов";
-			this->oldChats->UseVisualStyleBackColor = true;
-			// 
-			// usersOnline
-			// 
-			this->usersOnline->Location = System::Drawing::Point(25, 230);
-			this->usersOnline->Name = L"usersOnline";
-			this->usersOnline->Size = System::Drawing::Size(110, 40);
-			this->usersOnline->TabIndex = 2;
-			this->usersOnline->Text = L"Онлайн";
-			this->usersOnline->UseVisualStyleBackColor = true;
-			// 
-			// exit
-			// 
-			this->exit->Location = System::Drawing::Point(25, 325);
-			this->exit->Name = L"exit";
-			this->exit->Size = System::Drawing::Size(110, 40);
-			this->exit->TabIndex = 3;
-			this->exit->Text = L"Выход";
-			this->exit->UseVisualStyleBackColor = true;
+			this->ChatUsers->AcceptsTab = true;
+			this->ChatUsers->Location = System::Drawing::Point(485, 18);
+			this->ChatUsers->Multiline = true;
+			this->ChatUsers->Name = L"ChatUsers";
+			this->ChatUsers->Size = System::Drawing::Size(208, 88);
+			this->ChatUsers->TabIndex = 5;
 			// 
 			// infoTextBox
 			// 
@@ -169,42 +195,42 @@ namespace ClientWinForms {
 			this->infoTextBox->Size = System::Drawing::Size(544, 389);
 			this->infoTextBox->TabIndex = 4;
 			// 
-			// newMessage
+			// exit
 			// 
-			this->newMessage->Location = System::Drawing::Point(10, 326);
-			this->newMessage->Multiline = true;
-			this->newMessage->Name = L"newMessage";
-			this->newMessage->Size = System::Drawing::Size(575, 67);
-			this->newMessage->TabIndex = 0;
+			this->exit->Location = System::Drawing::Point(25, 325);
+			this->exit->Name = L"exit";
+			this->exit->Size = System::Drawing::Size(110, 40);
+			this->exit->TabIndex = 3;
+			this->exit->Text = L"Выход";
+			this->exit->UseVisualStyleBackColor = true;
 			// 
-			// oldMessages
+			// usersOnline
 			// 
-			this->oldMessages->Enabled = false;
-			this->oldMessages->Location = System::Drawing::Point(10, 15);
-			this->oldMessages->Multiline = true;
-			this->oldMessages->Name = L"oldMessages";
-			this->oldMessages->ReadOnly = true;
-			this->oldMessages->Size = System::Drawing::Size(575, 300);
-			this->oldMessages->TabIndex = 1;
+			this->usersOnline->Location = System::Drawing::Point(25, 230);
+			this->usersOnline->Name = L"usersOnline";
+			this->usersOnline->Size = System::Drawing::Size(110, 40);
+			this->usersOnline->TabIndex = 2;
+			this->usersOnline->Text = L"Онлайн";
+			this->usersOnline->UseVisualStyleBackColor = true;
 			// 
-			// sendMessage
+			// oldChats
 			// 
-			this->sendMessage->Location = System::Drawing::Point(596, 327);
-			this->sendMessage->Name = L"sendMessage";
-			this->sendMessage->Size = System::Drawing::Size(106, 65);
-			this->sendMessage->TabIndex = 2;
-			this->sendMessage->Text = L"Отправить";
-			this->sendMessage->UseVisualStyleBackColor = true;
+			this->oldChats->Location = System::Drawing::Point(25, 135);
+			this->oldChats->Name = L"oldChats";
+			this->oldChats->Size = System::Drawing::Size(110, 40);
+			this->oldChats->TabIndex = 1;
+			this->oldChats->Text = L"Список чатов";
+			this->oldChats->UseVisualStyleBackColor = true;
 			// 
-			// usersInChat
+			// newChat
 			// 
-			this->usersInChat->Enabled = false;
-			this->usersInChat->Location = System::Drawing::Point(596, 14);
-			this->usersInChat->Multiline = true;
-			this->usersInChat->Name = L"usersInChat";
-			this->usersInChat->ReadOnly = true;
-			this->usersInChat->Size = System::Drawing::Size(105, 300);
-			this->usersInChat->TabIndex = 3;
+			this->newChat->Location = System::Drawing::Point(25, 40);
+			this->newChat->Name = L"newChat";
+			this->newChat->Size = System::Drawing::Size(110, 40);
+			this->newChat->TabIndex = 0;
+			this->newChat->Text = L"Новый чат";
+			this->newChat->UseVisualStyleBackColor = true;
+			this->newChat->Click += gcnew System::EventHandler(this, &TheChatWindow::NewChat_Click);
 			// 
 			// TheChatWindow
 			// 
@@ -214,14 +240,22 @@ namespace ClientWinForms {
 			this->Controls->Add(this->tabControl1);
 			this->Name = L"TheChatWindow";
 			this->Text = L"TheChatWindow";
+			this->tabPage2->ResumeLayout(false);
+			this->tabPage2->PerformLayout();
 			this->tabControl1->ResumeLayout(false);
 			this->tabPage1->ResumeLayout(false);
 			this->tabPage1->PerformLayout();
-			this->tabPage2->ResumeLayout(false);
-			this->tabPage2->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
-	};
+	private: System::Void NewChat_Click(System::Object^ sender, System::EventArgs^ e) {
+		if (ChatUsers->Text->Length == 0 || ChatUsers->Text=="Введите пользователей") {
+			ChatUsers->Text = "Введите пользователей";
+		}
+		else {
+			tabControl1->SelectedIndex = 1;
+		}
+	}
+};
 }
